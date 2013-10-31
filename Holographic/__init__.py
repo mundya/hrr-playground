7fac4b6409e59710f87cbaac653873b6013c3498
Andrew Mundy <andrew.mundy@ieee.org>
2013-10-31 14:57:31 +0000

    Removed compiled py files, modified __init__.

---
+++ Holographic/__init__.py
@@ -0,0 +1,6 @@
+# Python Holographic Reduced Representation Library
+# -------------------------------------------------
+# (C) Copyright Andrew Mundy 2013
+
+import Symbol
+from utils import vec_generate, vec_convolve_circular, vec_exponentiate
